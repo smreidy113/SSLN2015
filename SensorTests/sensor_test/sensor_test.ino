8047f22136b027f5f1157f2b750c76f4deede73a
Sean <smreidy113@gmail.com>
2014-12-04 00:19:17 -0500

    sensors and triangulation

--- SensorTests/sensor_test/sensor_test.ino
+++ SensorTests/sensor_test/sensor_test.ino
@@ -1,8 +1,26 @@
+#define DIST_BETWEEN_SENSORS 6.4
+
+#include <VirtualWire.h>
+#include <VirtualWire_Config.h>
+#include <stdlib.h>
+
+double getLocInfo(int d1, int d2) {
+  double beta = acos((pow(d2,2)+pow(DIST_BETWEEN_SENSORS,2)-pow(d1,2))/(2*d2*DIST_BETWEEN_SENSORS));
+  double d = sqrt(pow(sin(beta),2)*pow(d2,2)+pow(DIST_BETWEEN_SENSORS/2-cos(beta)*d2,2));
+  return d;
+}
+
+double getOrientationInfo(double d, int d1, int d2) {
+  double beta = acos((pow(d2,2)+pow(DIST_BETWEEN_SENSORS,2)-pow(d1,2))/(2*d2*DIST_BETWEEN_SENSORS));
+  double theta = 3.141592/2 - asin(sin(beta)*d2/d);
+  return theta;
+}
+
 int pinput1 = 2;
 int pinput2 = 3;
 int poutput = 13;
+unsigned long duration;
 unsigned long duration1;
-unsigned long duration2;
 
 void pulseOut(int pin, int us)
 {
@@ -13,28 +31,44 @@ void pulseOut(int pin, int us)
 }
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(4800);	  // Debugging only
+
+  // Initialise the IO and ISR
+  vw_set_ptt_inverted(true); // Required for DR3100
+  vw_setup(2400);	 // Bits per sec
   pinMode(pinput1, INPUT);
   pinMode(pinput2, INPUT);
   digitalWrite(pinput1, LOW);
   digitalWrite(pinput2, LOW);
 }
 
+int getTime(int pin) {
+  pinMode(pin, OUTPUT);
+  pulseOut(pin, 0);
+  pinMode(pin, INPUT);
+  digitalWrite(pin, LOW);
+  return pulseIn(pin, HIGH)/29;
+}
+
+
+
 void loop() {
-  pinMode(pinput1, OUTPUT);
-  pulseOut(pinput1, 0);
-  pinMode(pinput1, INPUT);
-  digitalWrite(pinput1, LOW);
-  duration = pulseIn(pinput1, HIGH)/29;
-  pinMode(pinput2, OUTPUT);
-  pulseOut(pinput2, 0);
-  pinMode(pinput2, INPUT);
-  digitalWrite(pinput2, LOW);
-  duration1 = pulseIn(pinput2, HIGH)/29;
+  const char *msg = "p";
+  digitalWrite(13, HIGH);
+  vw_send((uint8_t *)msg, strlen(msg));
+  //vw_wait_tx(); // Wait until the whole message is gone
+  digitalWrite(13, LOW);
+  duration = getTime(pinput1);
+  vw_send((uint8_t *)msg, strlen(msg));
+  duration1 = getTime(pinput2);
+  double dist = getLocInfo(duration, duration1);
+  double ang = getOrientationInfo(dist, duration, duration1);
   Serial.print(duration);
+  Serial.print("\t");
+  Serial.print(duration1);
+  Serial.print("\tTotal Distance: ");
+  Serial.print(dist);
+  Serial.print("\tOrientation: ");
+  Serial.print(ang);
   Serial.print("\n");
-  //digitalWrite(pinput1, HIGH);
-  delay(500);
-  //digitalWrite(pinput1, LOW);
-  //delay(500);
 }
--- WirelessTests/wireless_test_send/wireless_test_send.ino
+++ WirelessTests/wireless_test_send/wireless_test_send.ino
@@ -1,20 +1,38 @@
+#include <VirtualWire.h>
+#include <VirtualWire_Config.h>
+
 /*
 * Simple Transmitter Code
 * This code simply counts up to 255
 * over and over
 * (TX out of Arduino is Digital Pin 1)
 */
+
+
 byte counter;
+int pin = 13;
+
+char *controller;
 
 void setup(){
-//2400 baud for the 434 model
-Serial.begin(2400);
+  pinMode(1,OUTPUT);
+  //2400 baud for the 434 model
+  vw_set_ptt_inverted(true);
+  vw_set_tx_pin(12);
+  vw_setup(4800);
   counter = 0;
 }
 void loop(){
 //send out to transmitter
-  Serial.print(counter);
-  counter++;
-  delay(10);
+  controller="1"  ;
+vw_send((uint8_t *)controller, strlen(controller));
+vw_wait_tx(); // Wait until the whole message is gone
+digitalWrite(13,1);
+delay(2000);
+controller="0"  ;
+vw_send((uint8_t *)controller, strlen(controller));
+vw_wait_tx(); // Wait until the whole message is gone
+digitalWrite(13,0);
+delay(2000);
 }
 
